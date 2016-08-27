a0b29e6a0a7131b30da0bb68f18c272f996c3220
nrod80 <nicholas.rodriguez@duke.edu>
2016-08-27 12:54:38 -0500

    working to add a single page to db

--- crawler.js
+++ crawler.js
@@ -1,10 +1,60 @@
+const Page = require('./index.js').Page
+const Domain = require('./index.js').Domain
+const Sequelize = require('sequelize');
 const RequestTurtle = require('request-turtle');
-const turtle = new RequestTurtle({ limit: 300 }); // limit rate to 300ms. this is the default
+const cheerio = require('cheerio')
+const turtle = new RequestTurtle({
+  limit: 1000
+}); // limit rate to 300ms. this is the default
+
+let urlsToCrawl = ['https://en.wikipedia.org/wiki/Cheerios?oldformat=true']
 
 
-for(var i = 0; i < Math.pow(10, 1000); i++) {
-  turtle.request({ method: "POST", uri: 'http://foo.org' })
-    .then(function(responseBody) {
-      // safely make all requests
+function crawlPage(url) {
+
+  turtle.request({
+    method: "GET",
+    uri: url,
+    resolveWithFullResponse: true
+  }).then(function(fullResponse) {
+
+    let $ = cheerio.load(fullResponse.body);
+
+    let pageObj = {};
+    let domainObj = {
+      name: fullResponse.request.uri.host
+    }
+
+    pageObj['status'] = fullResponse.request.responseContent.statusCode;
+    pageObj['title'] = $('title').text();
+    pageObj['textContent'] = $('body').text()
+    pageObj['url'] = fullResponse.request.uri.href
+
+    var page = Page.create(pageObj)
+      // var domain = Domain.build(domainObj)
+      // page.addDomain(domain, domainObj)
+      // page.save()
+
+    let allA = $('#bodyContent').find('a')
+    for (let i = 0; i < allA.length; i++) {
+      let url = allA[i].attribs.href
+      if (url[0] === '#') {
+        continue;
+      }
+      if (url.slice(0, 6) === '/wiki/') {
+        url = 'http://en.wikipedia.org'.concat(url)
+      }
+      urlsToCrawl.push(url);
+    }
+  })
+    .catch(function(error) {
+      console.log('you fucked up cuz', error)
     });
+
+  // urlsToCrawl.forEach(function(url) {
+  //   crawlPage(url)
+  // })
 }
+urlsToCrawl.forEach(function(url) {
+  crawlPage(url)
+})
